4e33f5aca13135014fb555803684deee5de34041
Ömer Aydin <64536242+Omer29-create@users.noreply.github.com>
2022-09-04 20:13:44 +0200

    Add files via upload

---
+++ src/main/java/customer/exception/MailServerUnavailableException.java
@@ -0,0 +1,8 @@
+package customer.exception;
+
+public class MailServerUnavailableException extends RuntimeException {
+
+    public MailServerUnavailableException(String message) {
+        super(message);
+    }
+}
