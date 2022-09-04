230d325dc00d073b60886f15632fd8c99933cc87
Ömer Aydin <64536242+Omer29-create@users.noreply.github.com>
2022-09-04 20:14:12 +0200

    Update BilgilendirmeService.java

--- src/main/java/customer/BilgilendirmeService.java
+++ src/main/java/customer/BilgilendirmeService.java
@@ -1,8 +1,15 @@
 package customer;
 
+import customer.exception.MailServerUnavailableException;
+
 public class BilgilendirmeService {
 
     public void yeniKayitMailGonder(Musteri musteri) {
+
         System.out.println("Mail gonderildi");
+    }
+
+    public void haftalikMailGonder() {
+        throw new MailServerUnavailableException("mail hata veriyor");
     }
 }
