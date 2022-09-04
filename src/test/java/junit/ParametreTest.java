5881d127104a9d0e8de8904b46167e2cb9c7cd35
Ömer Aydin <64536242+Omer29-create@users.noreply.github.com>
2022-09-04 12:16:18 +0200

    Update ParametreTest.java

--- src/test/java/junit/ParametreTest.java
+++ src/test/java/junit/ParametreTest.java
@@ -2,14 +2,40 @@ package junit;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import ornek.HesapMakinesi;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
 public class ParametreTest {
 
     private HesapMakinesi hesapMakinesi = new HesapMakinesi();
 
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+            {10, 2, 20},
+            {20, 2, 40},
+            {5, 2, 10},
+            {24, 2, 48},
+            {6, 2, 12}});
+    }
+    private int en;
+    private int boy;
+    private int toplamMetreKare;
+
+    public ParametreTest(int en, int boy, int toplamMetreKare) {
+        this.en = en;
+        this.boy = boy;
+        this.toplamMetreKare = toplamMetreKare;
+    }
+
     @Test
     public void testMetreKareHesapla() throws Exception {
-        Assert.assertEquals(20, hesapMakinesi.metreKareHesap(10,2));
+        Assert.assertEquals(toplamMetreKare, hesapMakinesi.metreKareHesap(en, boy));
     }
 }
+
