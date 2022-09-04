0bba3674d70c9b00c75195503ba6ff4b06805f4d
Ömer Aydin <64536242+Omer29-create@users.noreply.github.com>
2022-09-04 11:47:25 +0200

    Update ParametreTest.java

--- src/test/java/junit/ParametreTest.java
+++ src/test/java/junit/ParametreTest.java
@@ -1,4 +1,15 @@
 package junit;
 
+import org.junit.Assert;
+import org.junit.Test;
+import ornek.HesapMakinesi;
+
 public class ParametreTest {
+
+    private HesapMakinesi hesapMakinesi = new HesapMakinesi();
+
+    @Test
+    public void testMetreKareHesapla() throws Exception {
+        Assert.assertEquals(20, hesapMakinesi.metreKareHesap(10,2));
+    }
 }
